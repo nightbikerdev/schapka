6debac747632a58b9bf483edcff40c4f8ab31547
Aleksandra Skonieczna <alskonieczna@gmail.com>
2024-03-11 20:57:19 +0100

    feat: add color schema and icon to app

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -4,6 +4,29 @@ void main() {
   runApp(const MyApp());
 }
 
+class AppColors {
+  static const Color customYellow = Color(0xFFfac564); // #fac564
+  static const Color customBlack = Color(0xFF1f1f1f); // #1f1f1f
+  // Add more colors as needed
+}
+
+class AppColorScheme {
+  static const ColorScheme colorScheme = ColorScheme(
+    primary: AppColors.customYellow,
+    secondary: AppColors.customBlack,
+    surface: Colors.white,
+    background: AppColors.customYellow,
+    error: Colors.red,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    onSurface: Colors.black,
+    onBackground: Colors.black,
+    onError: Colors.white,
+    brightness: Brightness.light,
+    primaryContainer: AppColors.customBlack,
+  );
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,26 +34,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Flutter Demo Schapka',
+      theme: ThemeData.from(
+          colorScheme: AppColorScheme.colorScheme, useMaterial3: true),
+      // theme: ThemeData(
+      //   // This is the theme of your application.
+      //   //
+      //   // TRY THIS: Try running your application with "flutter run". You'll see
+      //   // the application has a purple toolbar. Then, without quitting the app,
+      //   // try changing the seedColor in the colorScheme below to Colors.green
+      //   // and then invoke "hot reload" (save your changes or press the "hot
+      //   // reload" button in a Flutter-supported IDE, or press "r" if you used
+      //   // the command line to start the app).
+      //   //
+      //   // Notice that the counter didn't reset back to zero; the application
+      //   // state is not lost during the reload. To reset the state, use hot
+      //   // restart instead.
+      //   //
+      //   // This works for code too, not just values: Most code changes can be
+      //   // tested with just a hot reload.
+      //    colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+      //   useMaterial3: true,
+      // ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
